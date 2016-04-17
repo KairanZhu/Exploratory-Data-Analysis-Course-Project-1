7e78cc79fd365919f9bea3c3cf2677fe8177150b
KairanZhu <kairan.zhu@yahoo.com>
2016-04-17 17:36:34 -0400

    Update Plot3.R

--- Course Project 1/Plot3.R
+++ Course Project 1/Plot3.R
@@ -1,3 +1,12 @@
+# Data preparation
+data <- read.csv("household_power_consumption.txt", sep=";", 
+                 na.strings = "?", stringsAsFactors=FALSE)
+data <- na.omit(data)
+data$Date <- as.Date(data$Date, format = "%d/%m/%Y")
+subset <- subset(data, subset = (Date >= "2007-02-01" & Date <= "2007-02-02"))
+subset$DateTime <- paste(subset$Date,subset$Time)
+subset$DateTime <- as.POSIXct(subset$DateTime)
+
 # Plot 3
 par(mfrow=c(1,1))
 
